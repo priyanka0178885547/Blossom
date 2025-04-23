62d93fa89ee34fe3e7c22d1a3b191032f1c395fb
Manasa Morthad <morthadmanasa@gmail.com>
2025-04-23 10:04:47 +0530

    updated styling

--- frontend/src/components/wishlistPage.js
+++ frontend/src/components/wishlistPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./Wishlist.css";
+import "./wishlist.css";
 import { useNavigate } from "react-router-dom";
 
 const WishlistPage = () => {
